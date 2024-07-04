4b78634353c683f8349740de9579bfae6b02fe89
ali-alaalem <Meemo2002a@gmail.com>
2024-07-04 12:49:28 +0300

    modification on the dificulty buttons

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
 /*-------------- Constants -------------*/
-const btn = document.querySelector("button");
+const easybtn = document.querySelector("#Easy");
+const midbtn = document.querySelector("#Mid");
+const hardbtn = document.querySelector("#Hard");
 const div = document.querySelector(".Container");
+const message = document.querySelector("p");
 /*---------- Variables (state) ---------*/
-let clickedCounter = 0;
 let randomNum = 0;
+let easyClicked = false;
+let midClicked = false;
+let hardClicked = false;
 let randomNumArr = [];
-let img = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let easyimg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let midimg = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+let hardimg = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30,
+];
 /*----- Cached Element References  -----*/
 
 /*-------------- Functions -------------*/
@@ -40,15 +52,54 @@ function randomNumGenerater() {
     }
   }
 }
-function abbendChild() {
-  for (let i = 0; i < img.length; i++) {
-    img[i] = document.createElement("img");
-    img[i].src = "./images/Q.jpeg";
-    div.appendChild(img[i]);
+function easyabbendChild() {
+  if (easyClicked === false) {
+    removeAllImage();
+    for (let i = 0; i < easyimg.length; i++) {
+      easyimg[i] = document.createElement("img");
+      easyimg[i].src = "./images/Q.jpeg";
+      div.appendChild(easyimg[i]);
+    }
+    easyClicked = true;
+    midClicked = false;
+    hardClicked = false;
   }
-  clickedCounter++;
+}
+function midabbendChild() {
+  if (midClicked === false) {
+    removeAllImage();
+    for (let i = 0; i < midimg.length; i++) {
+      midimg[i] = document.createElement("img");
+      midimg[i].src = "./images/Q.jpeg";
+      div.appendChild(midimg[i]);
+    }
+    midClicked = true;
+    easyClicked = false;
+    hardClicked = false;
+  }
+}
+function hardabbendChild() {
+  if (hardClicked === false) {
+    removeAllImage();
+    for (let i = 0; i < hardimg.length; i++) {
+      hardimg[i] = document.createElement("img");
+      hardimg[i].src = "./images/Q.jpeg";
+      div.appendChild(hardimg[i]);
+    }
+    hardClicked = true;
+    easyClicked = false;
+    midClicked = false;
+  }
+}
+
+function removeAllImage() {
+  while (div.firstChild) {
+    div.removeChild(firstChild);
+  }
 }
 
 /*----------- Event Listeners ----------*/
 document.addEventListener("keydown", handelKeyDown);
-btn.addEventListener("click", abbendChild);
+easybtn.addEventListener("click", easyabbendChild);
+midbtn.addEventListener("click", midabbendChild);
+hardbtn.addEventListener("click", hardabbendChild);
